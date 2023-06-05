4c024417a14afdd043bfff8a2b2240364654c941
Hihthei <cbouillon@et.esiea.fr>
2023-06-05 16:10:28 +0200

    Firefly.cpp / .h V1 CONTINUE

--- SPW/Firefly.cpp
+++ SPW/Firefly.cpp
@@ -10,6 +10,45 @@ Firefly::~Firefly()
 {
 }
 
+void Firefly::Start()
+{
+    //Joue l'animation par défaut
+    //TODO
+
+    //Crée le corps
+    PE_World &world = m_scene.GetWorld();
+    PE_BodyDef bodyDef;
+    bodyDef.type = PE_BodyType::DYNAMIC;
+    bodyDef.position = GetStartPosition() + PE_Vec2(5.0f, 10.0f);
+    bodyDef.name = (char*)"Firefly";
+    bodyDef.damping.SetZero();
+    PE_Body* body = world.CreateBody(bodyDef);
+    SetBody(body);
+
+    // Création du collider
+    PE_ColliderDef colliderDef;
+
+    PE_CapsuleShape capsule(PE_Vec2(0.09f, 0.23f), PE_Vec2(0.09f, 0.78f), 0.23f);
+    colliderDef.friction = 2.5f;
+    colliderDef.filter.categoryBits = CATEGORY_PLAYER;
+    colliderDef.shape = &capsule;
+    PE_Collider* collider = body->CreateCollider(colliderDef);
+
+}
+
+void Firefly::Update()
+{
+    //TODO
+}
+
+void Firefly::Render()
+{
+}
+
+void Firefly::OnRespawn()
+{
+}
+
 void Firefly::Collect(Collectable* Firefly)
 {
 }
--- SPW/Firefly.h
+++ SPW/Firefly.h
@@ -10,7 +10,10 @@ public:
     Firefly(Scene& scene, Layer layer);
     virtual ~Firefly();
 
+    virtual void Start() override;
+    virtual void Update() override;
     virtual void Render() override;
+    virtual void OnRespawn() override;
 
     virtual void Collect(Collectable* Firefly);
 };
