ca28607991bbe98aad8c41170f819e48df4ee09f
Hihthei <cbouillon@et.esiea.fr>
2023-06-05 23:12:05 +0200

    Firefly V1 CP 1

--- SPW/Firefly.cpp
+++ SPW/Firefly.cpp
@@ -12,28 +12,35 @@ Firefly::~Firefly()
 
 void Firefly::Start()
 {
-    //Joue l'animation par défaut
-    //TODO
 
-    //Crée le corps
-    PE_World &world = m_scene.GetWorld();
+    SetToRespawn(true);
+
+    // Joue l'animation par défaut
+    m_animator.PlayAnimation("Idle");
+
+    // Crée le corps
+    PE_World& world = m_scene.GetWorld();
     PE_BodyDef bodyDef;
-    bodyDef.type = PE_BodyType::DYNAMIC;
-    bodyDef.position = GetStartPosition() + PE_Vec2(5.0f, 10.0f);
-    bodyDef.name = (char*)"Firefly";
+    bodyDef.type = PE_BodyType::STATIC;
+    bodyDef.position = GetStartPosition() + PE_Vec2(0.5f, 0.0f);
+    bodyDef.name = "Firefly";
     bodyDef.damping.SetZero();
     PE_Body* body = world.CreateBody(bodyDef);
     SetBody(body);
 
-    // Création du collider
+    // Crée le collider
+    PE_CircleShape circle(PE_Vec2(0.0f, 0.15f), 0.15f);
     PE_ColliderDef colliderDef;
-
-    PE_CapsuleShape capsule(PE_Vec2(0.09f, 0.23f), PE_Vec2(0.09f, 0.78f), 0.23f);
-    colliderDef.friction = 2.5f;
-    colliderDef.filter.categoryBits = CATEGORY_PLAYER;
-    colliderDef.shape = &capsule;
+    colliderDef.friction = 0.0f;
+    colliderDef.filter.categoryBits = CATEGORY_COLLECTABLE;
+    colliderDef.filter.maskBits = CATEGORY_PLAYER;
+    colliderDef.shape = &circle;
     PE_Collider* collider = body->CreateCollider(colliderDef);
 
+    // Endort le corps
+    // Permet d'optimiser le calcul de la physique,
+    // seuls les corps proches du joueur sont simulés
+    body->SetAwake(false);
 }
 
 void Firefly::Update()
